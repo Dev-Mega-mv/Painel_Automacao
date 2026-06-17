d5bcb016398a4a2d297f3f2746eebbb6f1f2f8c9
Emanuel Filipe <emanuelmanu45@gmail.com>
2026-06-17 14:15:19 -0300

    refactor: remove legacy automation import compatibility shims

--- automacao_ativador.py
+++
@@ -1,8 +0,0 @@
-"""Compatibilidade com o import legado.
-
-O codigo principal foi movido para automacoes.cielo_cardse.
-"""
-
-from automacoes.cielo_cardse import WebAppAtivador, automation_status
-
-__all__ = ['WebAppAtivador', 'automation_status']
--- automacao_extrator.py
+++
@@ -1,8 +0,0 @@
-"""Compatibilidade com o import legado.
-
-O codigo principal foi movido para automacoes.extrator.
-"""
-
-from automacoes.extrator import WebAppExtrator, extraction_status
-
-__all__ = ['WebAppExtrator', 'extraction_status']
